14bf807807e86aba06a906cf9aea692ae792b536
Tergrad <142720985+Tergrad@users.noreply.github.com>
2023-09-13 22:10:46 +0200

    Grunsätzliche Funktionalität geht :)

--- Main.js
+++ Main.js
@@ -25,35 +25,41 @@ function getCitationPatterns(style) {
 
     if (style === "APA") {
         console.log("[Setting APA Patterns]");
-        patterns.citation = new RegExp("^\\w+,\\s\\w+\\.", "gm"); // Insert your complex regex here
+        patterns.citation = new RegExp("\\((\\w+)\\s(\\d{4})(?:,\\s(\\d+))?\\)", "gm");
         patterns.reference = new RegExp("^\\w+,\\s\\w+\\.", "gm");
         console.log("[APA Patterns Set Successfully]");
-        
+    } else {
+        console.error("Unrecognized style:", style);
+        throw new Error("Unrecognized citation style provided.");
     }
 
+    console.log("[Returning Patterns for]:", style);
     return patterns;
 }
 
-
-// FULL REGEX
-// "(?<original>(?:(?:\(|\; ?)(?<author>(?=[^\n\(]*,[0-9 ]*[^\(\)]*\))(?!:[^\n\(\)\[\]]+\[.*\].*(?:\d{4}[a-z]?|[ns]\.\s?d\.|\d{4}[-–]\d{4}|\d{4}\, ?\w+ \d{1,2}))[^\n\(\)]*?(?:\b\p{Lu}[\p{Ll} \_0-9]+\b,\s+(?:\p{Lu}\.){1,3}\s+(?:(?:&|,|\.\.\.|)\s*)?)*(?:\((?:Eds?\.|Trad\.|Comp\.).*\))?)(?:,? ?(?<year>(?:\d{4}[a-z]?(?:, ?)?)+))(?:[, ]*pp?\.[\s]*(?<page>\d+[\-–]?\d*))?(?:, para\.[\s]*(?<paragraph>\d+))?))|(?:(?<pre>(?:[\p{Ll} \_0-9]*)( [\w]{1,6} )?)(?=\.?\p{Lu}\w+)(?<author1>(?<a1>(?:[^\n\(\)\.0-9]+)|(?:(?:[A-Z]\.\s?)?\b\p{Lu}[\p{Ll} \_0-9]+\b\s*et al\.? ?))?(?<a2>\b\p{Lu}[\p{Ll} \_0-9]+\b,\s+(?:\p{Lu}\.){1,3}\s+(?:(?:&|,|\.\.\.|)\s*)?)*(?:\((?:Eds?\.|Trad\.|Comp\.).*\))?)(\((?<year1>(?:\d{4}[a-z]?(?:, ?)?)+)\)))", "gmi"
 
 function processDocx(fileInput) {
     console.log("[Processing DOCX]");
     let reader = new FileReader();
 
+    console.log("[File Size]:", fileInput.files[0].size / 1024, "KB");  // Log the size of the file for diagnostic purposes.
+
     reader.onload = function(event) {
         console.log("[File Reader Onload]");
-        mammoth.extractRawText({ arrayBuffer: event.target.result })
-            .then(function(result) {
-                console.log("[Mammoth Text Extraction Success]");
-                console.log("Extracted text:", result.value); // Diagnostic log
-                displayResult(result);
-            })
-            .catch(function(err) {
-                console.log("[Mammoth Text Extraction Error]:", err);
-                handleError(err);
-            });
+        try {
+            mammoth.extractRawText({ arrayBuffer: event.target.result })
+                .then(function(result) {
+                    console.log("[Mammoth Text Extraction Success]");
+                    console.log("Extracted text:", result.value.substring(0, 100) + "..."); // Logging only a snippet of the extracted text for readability.
+                    displayResult(result);
+                })
+                .catch(function(err) {
+                    console.log("[Mammoth Text Extraction Error]:", err);
+                    handleError(err);
+                });
+        } catch (error) {
+            console.error("[Error in reader.onload]:", error);
+        }
     };
 
     reader.onerror = function(error) {
@@ -62,6 +68,7 @@ function processDocx(fileInput) {
 
     reader.readAsArrayBuffer(fileInput.files[0]);
 }
+
 
 function displayResult(result) {
     console.log("[Displaying Result]");
@@ -79,17 +86,66 @@ function checkReferences(text, style) {
     console.log("[Checking References]");
     const patterns = getCitationPatterns(style);
 
-    const inTextCitations = [...text.matchAll(patterns.citation)].map(match => match[1]);
-    const referenceList = [...text.matchAll(patterns.reference)].map(match => match[0]);
+    // Logging the patterns for diagnostic purposes.
+    console.log("[Patterns]:", patterns);
 
-    const missingReferences = inTextCitations.filter(cite => !referenceList.includes(cite));
+    try {
+        console.time("CitationMatching");
+        const inTextCitations = [...text.matchAll(patterns.citation)].map(match => match[0]);
+        console.timeEnd("CitationMatching");
 
-    document.getElementById("missingReferences").innerText = "Missing References: " + missingReferences.join(", ");
-    document.getElementById("allDetected").innerText = "All Detected Citations: " + inTextCitations.join(", ");
+        console.log("[In-Text Citations Found]:", inTextCitations.length);
 
-    const potentialPattern = /\(?[A-Za-z0-9]+, \d{4}\)?/g;
-    const potentialMisses = [...text.matchAll(potentialPattern)].map(match => match[0]).filter(cite => !inTextCitations.includes(cite));
+        console.time("ReferenceMatching");
+        const referenceList = [...text.matchAll(patterns.reference)].map(match => match[0]);
+        console.timeEnd("ReferenceMatching");
 
-    document.getElementById("potentialMisses").innerText = "Potential Misses: " + potentialMisses.join(", ");
+        console.log("[References Found]:", referenceList.length);
+
+        console.log("[Preparing to Display Missing References]");
+        const missingReferences = inTextCitations.filter(cite => !referenceList.includes(cite));
+
+        console.log("[Missing References]:", missingReferences);
+        document.getElementById("missingReferences").innerText = missingReferences.join(", ");
+
+        console.log("[All Detected Citations]:", inTextCitations);
+        document.getElementById("allDetected").innerText = inTextCitations.join(", ");
+
+        const potentialPattern = /\(?[A-Za-z0-9]+, \d{4}\)?/g;
+        const potentialMisses = [...text.matchAll(potentialPattern)].map(match => match[0]).filter(cite => !inTextCitations.includes(cite));
+
+        console.log("[Potential Misses]:", potentialMisses);
+        document.getElementById("potentialMisses").innerText = potentialMisses.join(", ");
+    } catch (error) {
+        console.error("[Error in checkReferences]:", error);
+    }
+
+    expandCollapsibles();
 }
+
+
+function expandCollapsibles() {
+    const contents = document.querySelectorAll(".results-section .content");
+    contents.forEach(content => {
+        content.style.maxHeight = "1000px";  // You can adjust this value
+    });
+}
+
+
+
+const collapsibleButtons = document.querySelectorAll(".collapsible");
+
+collapsibleButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+});
+
+
 console.log("[Script End - Event Listeners Waiting]");
